197bee64bdefe531fee8ab321311d374225e9a66
nati-sahlu <natansahlu5@gmai.com>
2025-01-05 18:31:11 +0300

    triel

--- models/user.py
+++ models/user.py
@@ -2,7 +2,7 @@
 """This is the user class"""
 from sqlalchemy.ext.declarative import declarative_base
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship
 from models.place import Place
 from models.review import Review
@@ -21,7 +21,7 @@ class User(BaseModel, Base):
     password = Column(String(128), nullable=False)
     first_name = Column(String(128))
     last_name = Column(String(128))
-    
-    # Relationships with cascade behavior (delete and delete-orphan)
-    places = relationship("Place", cascade='all, delete, delete-orphan', backref="user")
-    reviews = relationship("Review", cascade='all, delete, delete-orphan', backref="user")
+    places = relationship("Place", cascade='all, delete, delete-orphan',
+                          backref="user")
+    reviews = relationship("Review", cascade='all, delete, delete-orphan',
+                           backref="user")
